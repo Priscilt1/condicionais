9594782504318f5753d84282faadf533f3c63f09
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-11-17 11:25:13 -0300

    aplicando novos jeitos de fazer condicionais

---
+++ index.js
@@ -0,0 +1,27 @@
+function graduationFull(schooling) {
+    if (schooling == "EM") {
+     return "Ensino Medio Completo"
+    } if (schooling == "ES") {
+        return "Ensino Superior completo"
+    } if (schooling == "M") {
+        return "Mestrado"
+    } if (schooling == "D") {
+        return "Doutorado"
+    }
+}
+
+function graduation(schooling) {
+    const graduations = {
+      "EM": "Ensino Médio Completo",
+      "ES": "Ensino Superior Completo",
+      "M": "Mestrado",
+      "D": "Doutrado"
+    }
+
+    return graduations[schooling]
+}
+
+console.log(graduation("EM"))
+console.log(graduationFull("EM"))
+
+
